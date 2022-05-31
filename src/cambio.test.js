7542dd6fa2fc50edbbeaeb29ec0a8d58cddbcca7
Sebastian Salazar <sabestech@gmail.com>
2022-05-31 12:01:17 -0400

    cambio de moneda de 2 bs fallido

--- src/cambio.test.js
+++ src/cambio.test.js
@@ -4,4 +4,7 @@ describe("Cambio a monedas", () => {
   it("Si ponemos 1, deberia devolver 1", () => {
     expect(cambioAmonedas(1)).toEqual(1);
   });
+  it("Si ponemos 2, deberia devolver 1,1", () => {
+    expect(cambioAmonedas(2)).toEqual("1,1");
+  });
 });
