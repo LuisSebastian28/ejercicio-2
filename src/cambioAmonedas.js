0e96bbd358de2720bdbe239a2e7f259f83a2be8c
Sebastian Salazar <sabestech@gmail.com>
2022-05-31 12:02:09 -0400

    cambio de moneda de 2 bs aprobado

--- src/cambioAmonedas.js
+++ src/cambioAmonedas.js
@@ -1,5 +1,10 @@
-function cambioAmonedas(a, b) {
-    return 1;
+function cambioAmonedas(a) {
+    let resp=1;
+    if(a==2)
+    {
+        resp = "1,1"
+    }
+    return resp;
   }
   
   export default cambioAmonedas;
